e8004d99eeb74c7bcb29916e16f4f68294241e44
Victor Demchenko <praetoriandroid@gmail.com>
2016-12-05 15:35:55 +0300

    Handle all getSystemService() calls
    The replaced getSystemService() could be used by anyone for any system
    service, not only POWER_SERVICE. In all cases except POWER_SERVICE the
    the behaviour should not be changed.

--- app/src/main/java/com/example/gte/FakeManager.java
+++ app/src/main/java/com/example/gte/FakeManager.java
@@ -10,11 +10,12 @@ public class FakeManager {
         powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
     }
 
-    public static Object create(Context context, String name) {
-        if (!Context.POWER_SERVICE.equals(name)) {
-            throw new IllegalArgumentException("oops...");
+    public static Object getSystemService(Context context, String name) {
+        if (Context.POWER_SERVICE.equals(name)) {
+            return new FakeManager(context);
+        } else {
+            return context.getSystemService(name);
         }
-        return new FakeManager(context);
     }
 
     public FakeLock newWakeLock(int levelAndFlags, String tag) {
